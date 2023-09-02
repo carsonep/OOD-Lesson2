b8e99a82c9f1def81eab3965301c275b18823a8b
Carson Peterson <carsonepeterson@gmail.com>
2023-09-02 12:19:36 -0500

    Added coins

---
+++ model/Dime.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Dime extends Coin {
+    public static final int VALUE = 10;
+    public static final int SIZE = 20;
+
+    public Dime() {
+        super(VALUE, SIZE);
+    }
+}
